b0920dd17a22a640d200ee971cfb7830da593efd
Shu <2205951224@qq.com>
2022-07-22 14:41:46 +0800

    git first commit

---
+++ src/main/java/com/atguigu/controller/ControllerTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.controller;
+
+/**
+ * @author shubaolin
+ * @create 2022-07-22 14:40
+ */
+public class ControllerTest {
+    public static void main(String[] args) {
+        System.out.println("git hello!");
+    }
+}
