d4309781e653a28f4cad844dd71bf7187397ca49
Shu <2205951224@qq.com>
2022-07-22 14:45:45 +0800

    git second commit

--- src/main/java/com/atguigu/controller/ControllerTest.java
+++ src/main/java/com/atguigu/controller/ControllerTest.java
@@ -7,5 +7,6 @@ package com.atguigu.controller;
 public class ControllerTest {
     public static void main(String[] args) {
         System.out.println("git hello!");
+        System.out.println("git hello2!");
     }
 }
